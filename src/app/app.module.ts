a76188568eaae2b32cc917d857af4bb2456fe8f2
Johnny Hoang <johnnyhoaang@hotmail.com>
2020-11-17 14:54:14 +0100

    feat:Create pokemon page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 //Components
+import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FormsModule } from '@angular/forms';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { PokemonPageComponent } from './pages/pokemon-page/pokemon-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    DashboardComponent
+    DashboardComponent,
+    PokemonCardComponent,
+    PokemonPageComponent
   ],
   imports: [
     BrowserModule,
